cd25c851d1a0502e584e5cd6933ad64b80a5255f
Dev-Clyde <dev-clyde@Dev-Environment.myguest.virtualbox.org>
2024-05-21 01:11:10 +0800

    move input to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./Header";
-import InputGroup from "./InputGroup";
 import TableGroup from "./TableGroup";
 
-
 function App() {
-  return (
-    <>
-        <Header></Header>
-        <InputGroup></InputGroup>
-        <TableGroup></TableGroup>
-    </>
+    const yearMatrix = []
+    const leftRow= ["INITITAL INVESTMENT", "EXPECTED RETURN"];
+    const rightRow= ["ANNUAL INVESTMENT", "DURATION"];
+    const tableHead = ['Year', 'Investment value', 'Interest(Year)', 'Total Interest', 'Invested Capital'];
+
+
+    const [year, updateInvestments] = useState(yearMatrix);
+
+
+    function numberOfYears(numberOfYears) {
+        for (let i = 0; i < numberOfYears; i++) {
+            yearMatrix.push([i + 1, 0.0000, 0.0000, 0.0000, 0.0000]);
+        }
+    }
     
-  );
+    
+
+    function handleInputChange (event) {
+      console.log(event.target.name)
+    }
+
+    return (
+        <>
+            <Header />
+              <div className="input-group" id="user-input">
+                  <div>
+                      {leftRow.map((items, index) => (
+                              
+                          <div  key={index} id="row">
+                              <label>{items}</label>
+                              <input onChange={handleInputChange} type="number" defaultValue="0" min="0" name={items}/>
+                          </div>
+
+                      ))}
+                  </div>
+                  <div>
+                      {rightRow.map((items, index) => (
+                              
+                          <div key={index} id="row">
+                              <label>{items}</label>
+                              <input onChange={handleInputChange} type="number" defaultValue="0" min="0" name={items}/>
+                          </div>
+
+                      ))}
+                  </div>
+              </div>
+            <TableGroup tableHead={tableHead} yearRow={year} />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/TableGroup.jsx
+++ src/TableGroup.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import YearAndInvestments from './YearAndInvestments';  // Make sure to import the component
+import YearAndInvestments from './YearAndInvestments';
 
-export default function TableGroup() {
-    const tableHead = ['Year', 'Investment value', 'Interest(Year)', 'Total Interest', 'Invested Capital'];
-    const yearRow = [
-        ["1",'2315','0.9991','89123','89623'],
-        ["2",'2315','0.9991','89123','89623'],
-        ["3",'2315','0.9991','89123','89623'],
-        ["4",'2315','0.9991','89123','89623'],
-        ["5",'2315','0.9991','89123','89623'],
-        ["6",'2315','0.9991','89123','89623'],
-        ["7",'2315','0.9991','89123','89623'],
-        ["8",'2315','0.9991','89123','89623'],
-        ["9",'2315','0.9991','89123','89623'],
-        ["10",'2315','0.9991','89123','89623'],
-        ["11",'2315','0.9991','89123','89623'],
-        ["12",'2315','0.9991','89123','89623']
-    ];
-
+export default function TableGroup({tableHead, yearRow}) {
     return (
+
         <table id="result">
             <thead>
                 <tr>
