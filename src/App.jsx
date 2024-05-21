b6ffdae34b35dd73d7f041fd555c2c1b691e6a68
Dev-Clyde <dev-clyde@Dev-Environment.myguest.virtualbox.org>
2024-05-22 02:29:02 +0800

    initial complete implementation minor bugs left

--- src/App.jsx
+++ src/App.jsx
@@ -5,61 +5,92 @@ import TableGroup from "./TableGroup";
 import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
-    let yearMatrix = []
-    const leftRow= ["INITITAL INVESTMENT", "EXPECTED RETURN"];
+    let investmentMatrix = []
+    const leftRow= ["INITIAL INVESTMENT", "EXPECTED RETURN"];
     const rightRow= ["ANNUAL INVESTMENT", "DURATION"];
     const tableHead = ['Year', 'Investment value', 'Interest(Year)', 'Total Interest', 'Invested Capital'];
 
 
-    const [year, updateInvestments] = useState(yearMatrix);
+    const [matrix, updateMatrix] = useState(investmentMatrix);
+    const [initialInvestment, updateInitialInvestment] = useState('');
+    const [expectedReturn, updateExpectedReturn] = useState('');
+    const [annualInvestment, updateAnnualInvestment] = useState('');
+    const [duration, updateDuration] = useState('');
 
 
     function numberOfYears(numberOfYears) {
         let initialYearMatrix = [];
-        let investments = calculateInvestmentResults({
-          initialInvestment: 10000,
-          annualInvestment: 2000,
-          expectedReturn: 5, // 5% expected return
-          duration: numberOfYears,
-        });
 
-        for (let i = 0; i < numberOfYears; i++) {
-            initialYearMatrix.push([i + 1, 0.0000, 0.0000, 0.0000, 0.0000]);
+        for (let i = 0; i < Number(numberOfYears); i++) {
+            let investments = calculateInvestmentResults({
+                initialInvestment: Number(initialInvestment),
+                annualInvestment: Number(annualInvestment),
+                expectedReturn: Number(expectedReturn),
+                duration: Number(i+1),
+              });
+
+            let computedInterest = Number(investments[i]['interest']);
+            let computedTotalInterest = computeTotalInterest(investments);
+            let totalInvestedCapital = computeInvestedCapital(Number(initialInvestment), Number(annualInvestment), Number(i+1));
+            let computedInvestedValue = Number(investments[i]['valueEndOfYear']);
+
+            initialYearMatrix.push([i + 1, "$" + computedInvestedValue.toFixed(2), "$" + computedInterest.toFixed(2), "$" + computedTotalInterest.toFixed(2), "$" + totalInvestedCapital.toFixed(2)]);
         }
 
+        investmentMatrix = initialYearMatrix;
+    }
+    function computeTotalInterest (investments){
+        let computedTotalInterest = 0;
         for (let i in investments){
-          let initial = initialYearMatrix[i];
-        
-          initial[1] = investments[i]['valueEndOfYear']
-          initial[2] = investments[i]['interest']
-          initial[3] = investments[i]['interest']
-          initial[4] = investments[i]['annualInvestment']
-
-        };
-        console.log(initialYearMatrix)
-        yearMatrix = initialYearMatrix;
-    }
-    
-    
-    function mapInvestments(){
-      let v = calculateInvestmentResults({
-        initialInvestment: 10000,
-        annualInvestment: 2000,
-        expectedReturn: 5, // 5% expected return
-        duration: 10,
-      });
-      console.log(v);
+            computedTotalInterest += investments[i]['interest']
+        }
+        return Number(computedTotalInterest);
     }
 
-    mapInvestments()
+    function computeInvestedCapital(initialCapital, annualCapital, numberOfYears){
+        let totalInvestedCapital = initialCapital;
+        totalInvestedCapital += ((numberOfYears)*annualCapital)
+
+        return totalInvestedCapital;
+    }
+
+    
+    
 
     function handleInputChange (event) {
+
       const inputName =  event.target.name;
-      const inputValue = event.target.value;
+      const inputValue = Number(event.target.value);
       if(inputName == "DURATION"){
-          numberOfYears(event.target.value);
-          updateInvestments(yearMatrix);
-      } 
+          numberOfYears(inputValue);
+          updateMatrix(investmentMatrix);
+          updateDuration(inputValue)
+      }
+      if (inputName == "INITIAL INVESTMENT") {
+        updateInitialInvestment(inputValue);
+        console.log(typeof inputValue)
+        if (duration != "") {
+            numberOfYears(duration);
+            updateMatrix(investmentMatrix);
+        }
+
+      }
+      if (inputName == "EXPECTED RETURN") {
+        updateExpectedReturn(inputValue);
+        if (duration != "") {
+            numberOfYears(duration);
+            updateMatrix(investmentMatrix);
+        }
+
+      }
+      if (inputName == "ANNUAL INVESTMENT") {
+        updateAnnualInvestment(inputValue);
+        if (duration != "") {
+            numberOfYears(duration);
+            updateMatrix(investmentMatrix);
+        }
+
+      }    
 
     }
 
@@ -72,7 +103,7 @@ function App() {
                               
                           <div  key={index} id="row">
                               <label>{items}</label>
-                              <input onChange={handleInputChange} type="number" defaultValue="0" min="0" name={items}/>
+                              <input onChange={handleInputChange} type="number" placeholder="0" min="0" name={items}/>
                           </div>
 
                       ))}
@@ -82,13 +113,13 @@ function App() {
                               
                           <div key={index} id="row">
                               <label>{items}</label>
-                              <input onChange={handleInputChange} type="number" defaultValue="0" min="0" name={items}/>
+                              <input onChange={handleInputChange} type="number" placeholder="0" min="0" name={items}/>
                           </div>
 
                       ))}
                   </div>
               </div>
-            <TableGroup tableHead={tableHead} yearRow={year} />
+            <TableGroup tableHead={tableHead} yearRow={matrix} />
         </>
     );
 }
